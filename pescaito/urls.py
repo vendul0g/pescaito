c9eb6200a6c97707701bd41bef7755bb13faed01
Alvaro Navarro <a.navarromartinez1@um.es>
2024-06-03 22:15:52 +0200

    Página principal añadida

--- pescaito/urls.py
+++ pescaito/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from django.conf.urls.static import static  
+from django.conf.urls.static import static
+from main import views
 
 
 urlpatterns = [
+    path('', views.main_page, name='main_page'),
     path("admin/", admin.site.urls),
 ]
 
