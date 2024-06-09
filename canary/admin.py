6c2f78c50b5138fc34f67aa1a46dc2beac29f922
Alvaro Navarro <a.navarromartinez1@um.es>
2024-06-09 19:39:28 +0200

    Added alert to admin panel django

--- canary/admin.py
+++ canary/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Alert
+
 
 # Register your models here.
+@admin.register(Alert)
+class SimilarDomainAdmin(admin.ModelAdmin):
+    list_display = ["host"]
+    readonly_fields = (
+        "host",
+        "referrer",
+        "domain",
+        "ip",
+        "date",
+        "revisado",
+    )
